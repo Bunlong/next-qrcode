87832e2e8b6a05f811a1d6d13ce10de2817a0928
Bunlong <bunlong.van@gmail.com>
2022-01-30 23:44:30 +0700

    Define Image component (#18)

--- src/useQRCode.tsx
+++ src/useQRCode.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-// const QRCode = require('qrcode');
+const QRCode = require('qrcode');
+
+export interface Colors {
+  dark?: string;
+  light?: string;
+}
 
 export interface Options {
   type?: string;
@@ -11,31 +16,35 @@ export interface Options {
   color?: Colors;
 }
 
-export interface Colors {
-  dark?: string;
-  light?: string;
-}
-
-export interface Props {
+export interface IQRCode {
   text: string;
   options?: Options;
 }
 
 function useImageComponent() {
-  const ImageComponent = ({
+  const ImageComponent = <T extends HTMLImageElement>({
     text,
     options,
-  }: Props) => {
-    console.log('=====================');
-    console.log(text);
-    console.log(options);
-    console.log('=====================');
+  }: IQRCode) => {
+    const inputRef = React.useRef<T>(null);
 
-    return (
-      <>
-        {'Image'}
-      </>
+    React.useEffect(
+      function () {
+        if (inputRef && inputRef.current) {
+          QRCode.toDataURL(text, options, function (error: any, url: string) {
+            if (error) {
+              throw error;
+            }
+            if (inputRef.current instanceof HTMLImageElement) {
+              inputRef.current.src = url;
+            }
+          });
+        }
+      },
+      [text, options, inputRef],
     );
+
+    return <img ref={inputRef} />;
   };
 
   const Image = React.useMemo(() => ImageComponent, []);
@@ -43,29 +52,10 @@ function useImageComponent() {
   return Image;
 }
 
-function useCanvasComponent() {
-  const CanvasComponent = ({
-    text,
-    options,
-  }: Props) => {
-    return (
-      <>
-        {'Canvas'}
-      </>
-    );
-  };
-
-  const Canvas = React.useMemo(() => CanvasComponent, []);
-
-  return Canvas;
-}
-
 export function useQRCode() {
   const Image = useImageComponent();
-  const Canvas = useCanvasComponent();
 
   return {
     Image,
-    Canvas,
   };
 }
--- supports/create-next-app/pages/index.tsx
+++ supports/create-next-app/pages/index.tsx
@@ -1,27 +1,18 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 import { useQRCode } from 'next-qrcode';
 
 const Home: NextPage = () => {
-  const { Image, Canvas } = useQRCode<HTMLCanvasElement>({
-    text: 'https://github.com/bunlong/next-qrcode',
-    options: {
-      level: 'M',
-      margin: 7,
-      scale: 1,
-      width: 200,
-      color: {
-        dark: '#010599FF',
-        light: '#FFBF60FF',
-      },
-    },
-  });
+  const { Image, Canvas } = useQRCode();
 
   return (
-    <div className={styles.container}>
-      <Image text='' />
-      <Canvas text='' />
-    </div>
+    <>
+      <Image
+        text='Hello'
+      />
+      <Canvas
+        text='Hello'
+      />
+    </>
   )
 }
 
