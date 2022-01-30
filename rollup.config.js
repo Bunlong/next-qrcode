9a8791739a8c84744b3af58917351285a3f61c83
Bunlong <bunlong.van@gmail.com>
2022-01-30 23:14:25 +0700

    Resize to smaller (#18)

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
 import typescript from 'rollup-plugin-typescript2';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+
+// ======= FOR BUILDING NODE.JS PACKAGE =======
 // import builtins from 'builtin-modules'
-import { terser } from 'rollup-plugin-terser';
+// import resolve, { nodeResolve } from '@rollup/plugin-node-resolve';
+// ============================================
+
 import pkg from './package.json';
 
 export default {
@@ -19,25 +23,39 @@ export default {
       format: 'esm',
       exports: 'named',
     },
+    // {
+    //   file: `dist/${pkg.name}.min.es.js`,
+    //   format: 'es',
+    //   exports: 'named',
+    //   sourcemap: true
+    // },
+    // {
+    //   name: 'next-qrcode',
+    //   file: `dist/${pkg.name}.umd.js`,
+    //   format: 'umd',
+    //   exports: 'named',
+    //   globals: {
+    //     react: 'React',
+    //   },
+    // },
   ],
   // external: builtins,
-  // To use hook in development
-  // https://reactjs.org/warnings/invalid-hook-call-warning.html
-  external: ['react', 'react-dom'],
+  external: ['react', 'react-dom', 'qrcode'],
   plugins: [
     typescript({
       tsconfig: './tsconfig.json',
       clean: true,
     }),
     babel({
-      exclude: 'node_modules/**',
+      exclude: 'node_modules/**'
     }),
-    resolve({
-      preferBuiltins: true,
-    }),
+    // resolve({
+    //   preferBuiltins: true
+    // }),
+    // nodeResolve(),
     commonjs({
-      extensions: ['.js', '.ts', '.tsx'],
+      extensions: ['.js', '.ts', '.tsx']
     }),
-    terser(),
+    terser()
   ],
 };
